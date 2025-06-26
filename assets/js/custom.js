1ab14fded1cd3905b71c19908710de73d6626c93
ziaul-hoque4820 <ziaulhoque4820@gmail.com>
2025-06-26 20:55:22 +0600

    All types of design, functionality, animation have been completed and the project is half-completed.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -46,3 +46,51 @@ const swiper = new Swiper('.swiper', {
         },
     }
 });
+
+// -------------- show scroll up ---------------
+const scrollUp = () => {
+    const scrollUpBtn = document.getElementById('scroll-up')
+
+    if (this.scrollY >= 250) {
+        scrollUpBtn.classList.remove('-bottom-1/2')
+        scrollUpBtn.classList.add('bottom-4')
+    } else {
+        scrollUpBtn.classList.add('-bottom-1/2')
+        scrollUpBtn.classList.remove('bottom-4')
+    }
+}
+window.addEventListener('scroll', scrollUp);
+
+// CHANGE BACKGROUND HEADER 
+const scrollHeader = () => {
+    const header = document.getElementById('navbar')
+
+    if (this.scrollY >= 50) {
+        header.classList.add('border-b', 'border-yellow-500')
+    } else {
+        header.classList.remove('border-b', 'border-yellow-500')
+    }
+}
+window.addEventListener('scroll', scrollHeader);
+
+// Scrill Reveal Animation 
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 300,
+    reset: true
+});
+
+sr.reveal('.home_data, .about_top, .popular_top, .review_top, .review_swiper, .footer_icon, .footer_content, .copy_right');
+sr.reveal('.home_image', {delay: 500, scale: 0.5});
+
+sr.reveal('.service_card, .popular_card', {interval: 100});
+
+sr.reveal('.about_leaf', {delay: 1000, origin: 'right'});
+sr.reveal('.about_item_1-content, .about_item_2-img', {origin: 'right'});
+sr.reveal('.about_item_2-content, .about_item_1-img', {origin: 'right'});
+
+sr.reveal('.review_leaf, .footer_floral', {delay: 1000, origin: 'left'});
+
+
